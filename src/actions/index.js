45c7010fa121caf29852e32f38cb90f293f08418
Gabe Velasquez <gvelasquez@trackforce.com>
2019-02-06 17:45:08 -0800

    working redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,6 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  //bad approach!!!
+export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
-
-  return {
-    type: "FETCH_POSTS",
-    payload: response
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
