7941a0fd5baa9abaaddb4459e7a1af4aa2463109
Gabe Velasquez <gvelasquez@trackforce.com>
2019-02-11 18:21:41 -0800

    finished blog application

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("fetching posts");
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"), "userId");
+  console.log(userIds);
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -9,3 +18,9 @@ export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
